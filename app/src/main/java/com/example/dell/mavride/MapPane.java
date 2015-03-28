8988af94b63d534adf45be3b7bc3c99c1460db08
Liwanshi <liviraheja@gmail.com>
2015-03-28 02:07:35 -0500

    Map Activity

--- app/src/main/java/com/example/dell/mavride/MapPane.java
+++ app/src/main/java/com/example/dell/mavride/MapPane.java
@@ -3,6 +3,7 @@ package com.example.dell.mavride;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,17 +14,39 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 
-public class MapPane extends FragmentActivity implements OnMapReadyCallback {
+public class MapPane extends FragmentActivity implements OnMapReadyCallback, OnMarkerClickListener, OnMarkerDragListener {
+
+    private static final LatLng ERB = new LatLng(32.7330729,-97.1130619);
+    private static final LatLng NH = new LatLng(32.7324781,-97.1138454);
+    private static final LatLng UC = new LatLng(32.731654,-97.110998);
+    private static final LatLng CL = new LatLng(32.729659,-97.112958);
+    private static final LatLng UH = new LatLng(32.7290307,-97.1138804);
+    private static final LatLng KC = new LatLng(32.728002,-97.109622);
+    private static final LatLng PL = new LatLng(32.730395,-97.111811);
+    private static final LatLng LH = new LatLng(32.7290878,-97.1082488);
+    private static final LatLng WH = new LatLng(32.731606,-97.112588);
+    private static final LatLng AH = new LatLng(32.7310998,-97.1093611);
+
+    private GoogleMap googleMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        addGoogleMap();
+        // addLines();
+        addMarkers();
        // Parse.initialize(this, "Z1onpLDjhguG5Xerjh1sSzCm4T6o3tgQdN4TwjiM", "7WOxDqGAYaJulOnKZdLA9huezBWyB7OuOaBwjCQ0");
         MapFragment mapfragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
@@ -34,98 +57,77 @@ public class MapPane extends FragmentActivity implements OnMapReadyCallback {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
         dropdown.setAdapter(adapter);
     }
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        LatLng erb = new LatLng(32.7330729,-97.1130619);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(erb,16));
-        googleMap.addMarker(new MarkerOptions()
-                .title("ERB")
-                .snippet("ERB")
-                .position(erb));
 
-        LatLng nh = new LatLng(32.7324781,-97.1138454);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(nh,16));
-        googleMap.addMarker(new MarkerOptions()
-                .title("NH")
-                .snippet("Nedderman Hall")
-                .position(nh));
+    private void addGoogleMap() {
 
+        // check if we have got the googleMap already
+        if (googleMap == null) {
+            googleMap = ((SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map)).getMap();
+            googleMap.setOnMarkerClickListener(this);
+            googleMap.setOnMarkerDragListener(this);
+        }
+    }
+    private void addMarkers() {
+        if (googleMap != null) {
+            googleMap.setMyLocationEnabled(true);
 
-        LatLng uc = new LatLng(32.731654,-97.110998);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uc,16));
-        googleMap.addMarker(new MarkerOptions()
-                .title("UC")
-                .snippet("University Center")
-                .position(uc));
+            // a draggable marker with title and snippet
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ERB, 16));
+            googleMap.addMarker(new MarkerOptions().position(ERB)
+                    .title("ERB").snippet("Engineering Research Building")
+                    .flat(true)
+                    .draggable(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NH, 16));
+            googleMap.addMarker(new MarkerOptions().position(NH)
+                    .title("NH").snippet("Nedderman Hall")
+                    .flat(true)
+                    .draggable(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UC, 16));
+            googleMap.addMarker(new MarkerOptions().position(UC)
+                    .title("UC").snippet("University Center")
+                    .flat(true)
+                    .draggable(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CL, 16));
+            googleMap.addMarker(new MarkerOptions().position(CL)
+                    .title("CL").snippet("Central Library")
+                    .flat(true)
+                    .draggable(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(AH, 16));
+            googleMap.addMarker(new MarkerOptions().position(AH)
+                    .title("AH").snippet("Arlington Hall")
+                    .flat(true)
+                    .draggable(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UH, 16));
+            googleMap.addMarker(new MarkerOptions().position(UH)
+                    .title("UH").snippet("University Hall")
+                    .flat(true)
+                    .draggable(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KC, 16));
+            googleMap.addMarker(new MarkerOptions().position(KC)
+                    .title("KC").snippet("Kalpana Chawla Hall")
+                    .flat(true)
+                    .draggable(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(WH, 16));
+            googleMap.addMarker(new MarkerOptions().position(WH)
+                    .title("WH").snippet("Woolf Hall")
+                    .flat(true)
+                    .draggable(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(PL, 16));
+            googleMap.addMarker(new MarkerOptions().position(PL)
+                    .title("PL").snippet("Planeterium")
+                    .flat(true)
+                    .draggable(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LH, 16));
+            googleMap.addMarker(new MarkerOptions().position(LH)
+                    .title("LH").snippet("Lipscomb Hall")
+                    .flat(true)
+                    .draggable(true));
 
-        LatLng cl = new LatLng(32.729659,-97.112958);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cl,16));
-        googleMap.addMarker(new MarkerOptions()
-                .title("CL")
-                .snippet("Central library")
-                .position(cl));
-
-        LatLng uh = new LatLng(32.7290307,-97.1138804);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uh,16));
-        googleMap.addMarker(new MarkerOptions()
-                .title("UH")
-                .snippet("University Hall")
-                .position(uh));
-
-        LatLng kc = new LatLng(32.728002,-97.109622);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kc,16));
-        googleMap.addMarker(new MarkerOptions()
-                .title("KCH")
-                .snippet("Kalpana Chawla Hall")
-                .position(kc));
-
-        LatLng pl = new LatLng(32.730395,-97.111811);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pl,16));
-        googleMap.addMarker(new MarkerOptions()
-                .title("PL")
-                .snippet("Planeterium")
-                .position(pl));
-
-
-        LatLng lh = new LatLng(32.7290878,-97.1082488);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lh,16));
-        googleMap.addMarker(new MarkerOptions()
-                .title("LH")
-                .snippet("Lipscomb Hall")
-                .position(lh));
-
-        LatLng wh = new LatLng(32.731606,-97.112588);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(wh,16));
-        googleMap.addMarker(new MarkerOptions()
-                .title("WH")
-                .snippet("Woolf Hall")
-                .position(wh));
-
-
-        LatLng ah = new LatLng(32.7310998,-97.1093611);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ah,16));
-        googleMap.addMarker(new MarkerOptions()
-                .title("AH")
-                .snippet("Arlington Hall")
-                .position(ah));
+        }
 
 
 
-        /** googleMap.addMarker(new MarkerOptions()
-         .position(new LatLng(32.7310,-97.1150))
-         .title("Marker"));
-         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13))
-         googleMap.setMapType(googleMap.MAP_TYPE_TERRAIN);**/
 
     }
 
@@ -147,5 +149,52 @@ public class MapPane extends FragmentActivity implements OnMapReadyCallback {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        LatLng fromPosition = marker.getPosition();
+        Log.d(getClass().getSimpleName(), "Drag start at: " + fromPosition);
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+        LatLng toPosition = marker.getPosition();
+        LatLng fromPosition = marker.getPosition();
+
+        Toast.makeText(
+                getApplicationContext(),
+                "Marker " + marker.getTitle() + " dragged from " + fromPosition
+                        + " to " + toPosition, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        Log.i("GoogleMapActivity", "onMarkerClick");
+        LatLng src = marker.getPosition();
+        ParseObject map = new ParseObject("Location");
+        map.put("Source", src);
+        Toast.makeText(getApplicationContext(),
+                "Your current location is " + marker.getTitle(), Toast.LENGTH_LONG)
+                .show();
+        LatLng dest = marker.getPosition();
+        map.put("Destination", dest);
+        Toast.makeText(getApplicationContext(),
+                "Your destination is  " + marker.getTitle(), Toast.LENGTH_LONG)
+                .show();
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
