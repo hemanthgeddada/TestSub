e53ae225282b87e92020fdc54966c877cfcf88b6
Liwanshi <liviraheja@gmail.com>
2015-04-10 12:13:36 -0500

    setting

--- app/src/main/java/com/example/dell/mavride/Login.java
+++ app/src/main/java/com/example/dell/mavride/Login.java
@@ -125,20 +125,15 @@ public class Login extends Activity {
         return true;
     }
 
-    @Override
+   /* @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
-    }
+    }*/
     public void onClickForgotPassword(View view)
     {
         Intent intent=new Intent(this, ForgotPasswordFormActivity.class);
